435e320d65501a8ce7ef68419bd66e7244b2fc70
Manuel Gonzalez Gamito <mgonzalez.gamito@sevilla.servinformgroup.local>
2021-05-26 10:53:02 +0200

    cuart commit

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');  
-const app = express();  
-app.use(express.static(__dirname + '/dist'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
-});  
-app.listen(process.env.PORT || 4200);
+const express = require('express')
+const app = express()
+app.use(express.static('./dist/tasks'));
+app.get('/*', function(req, res) {
+ res.sendFile('index.html', {root: 'dist/tasks/'}
+ );
+});
+const port = 3500;
+app.listen(process.env.PORT || 4200), () => {
+ console.log(`Example app listening at http://localhost:${port}`)
+}
